06805ea39469e626ac9c4aa46a7fd8c66752e9be
barhoumiwajdi <wajdi.barhoumi26@gmail.com>
2023-09-05 15:54:29 +0100

    first commit

---
+++ src/app/user.ts
@@ -0,0 +1,10 @@
+export class User {
+    constructor(
+        public name: string,
+        public email: string,
+        public tel: number,
+        public topics: string,
+        public radioo: string,
+        public subscribe: boolean
+    ) { }
+}
